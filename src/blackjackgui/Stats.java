e0bafe2c638a61329a53a12706fc634a12e9980a
andrewtier <132238552+andrewtier@users.noreply.github.com>
2024-06-05 13:53:52 +1200

    Update 1.5 ,databaseConnection file, early stats

---
+++ src/blackjackgui/Stats.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blackjackgui;
+
+import java.sql.*;
+
+/**
+ *
+ * @author xndmo
+ */
+public class Stats {
+
+    public static final String URL = "jdbc:derby://localhost:1527/GamblersStats";
+    public static final String DATABASEUSERNAME = "abc";
+    public static final String PASSWORD = "123";
+     public static String username;
+    public static int plays, money, moneyBet, moneyWon, moneyLost, gamesWon, gamesLost;
+
+    /*    public Stats stats(username){
+    this.username = username;
+    this.plays = plays;
+    this.money = money;
+    
+    }*/
+    public static void getStats(String userKey) {
+        String sql = "SELECT * FROM ABC.PLAYERS WHERE username = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, DATABASEUSERNAME, PASSWORD); 
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, userKey);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                username = userKey;
+                plays = rs.getInt("plays");
+                money = rs.getInt("money");
+                moneyBet = rs.getInt("moneyBet");
+                moneyWon = rs.getInt("moneyWon");
+                moneyLost = rs.getInt("moneyLost");
+                gamesWon = rs.getInt("gamesWon");
+                gamesLost = rs.getInt("gamesLost");
+                /*
+                System.out.println("User: " + username);
+                System.out.println("Plays: " + plays);
+                System.out.println("Money: " + money);
+                System.out.println("Money Bet: " + moneyBet);
+                System.out.println("Money Won: " + moneyWon);
+                System.out.println("Money Lost: " + moneyLost);
+                System.out.println("Games Won: " + gamesWon);
+                System.out.println("Games Lost: " + gamesLost);*/
+            } else{
+                System.out.println("User was not found");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/blackjackgui/databaseTesting.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blackjackgui;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author xndmo
+ */
+public class databaseTesting {
+    
+
+    public static void main(String[] args) {
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter username");
+        input= scan.nextLine().trim();
+        
+        Stats.getStats(input);
+        printStats();
+        /*try (Connection conn = DatabaseConnection.getConnection(); 
+                Statement stmt = conn.createStatement()) {
+        
+        ResultSet rs = stmt.executeQuery("SELECT * FROM ABC.PLAYERS");
+        
+        while (rs.next()) {
+        String username = rs.getString("username");
+        int money = rs.getInt("money");
+        
+        System.out.println("user: " +username+" money:  "+money);
+        }
+        } catch (SQLException e) {
+        e.printStackTrace();
+        }*/
+        
+    }
+    
+    public static void printStats(){
+        System.out.println("User: " + Stats.username);
+                System.out.println("Plays: " + Stats.plays);
+                System.out.println("Money: " + Stats.money);
+                System.out.println("Money Bet: " + Stats.moneyBet);
+                System.out.println("Money Won: " + Stats.moneyWon);
+                System.out.println("Money Lost: " + Stats.moneyLost);
+                System.out.println("Games Won: " + Stats.gamesWon);
+                System.out.println("Games Lost: " + Stats.gamesLost);
+    }
+}
